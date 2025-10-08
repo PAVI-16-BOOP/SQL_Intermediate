279d1e742b3a6cda17aaf329101832267e3d9e17
Pavitra <vantoffactor15@gmail.com>
2025-10-08 19:42:39 +0530

    adding

--- SQL_Intermediate_Visualizations/2_cohort_analysis.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-	cohort_year ,
-	COUNT(DISTINCT(customerkey)) AS total_customers,
-	SUM(total_revenue) AS total_net_revenue,
-	SUM(total_revenue)/COUNT(DISTINCT(customerkey)) AS avg_customer_revenue
-FROM cohort_analysis 
-WHERE orderdate = first_purchase_date 
-GROUP BY 
-	cohort_year
